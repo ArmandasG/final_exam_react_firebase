baad4f2892e35f8412f7a429e0f16cb21a852f86
Armandas G <genysarmandas@gmail.com>
2023-04-20 16:11:12 +0300

    login form functional added

--- src/auth/LoginForm.jsx
+++ src/auth/LoginForm.jsx
@@ -1,8 +1,31 @@
+import { useFormik } from 'formik'
 import React from 'react'
 
-function LoginForm() {
+function LoginForm({onLogin}) {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    onSubmit: (values) => {
+      console.log('values ===', values);
+      onLogin(values)
+    }
+  })
   return (
-    <div>LoginForm</div>
+    <form onSubmit={formik.handleSubmit}>
+      <div>
+        <label className=''>Email</label>
+        <input className='' id='email' type="text" name='email' onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.email} />
+        {formik.touched.email && formik.errors.email ? (<div>{formik.errors.email}</div>) : null }
+      </div>
+      <div>
+        <label className=''>Password</label>
+        <input className='' id='password' type="password" name='password' onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.password} />
+        {formik.touched.password && formik.errors.password ? (<div>{formik.errors.password}</div>) : null }
+      </div>
+      <button type='submit'>Login</button>
+    </form>
   )
 }
 
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import LoginForm from '../auth/LoginForm'
+import { useNavigate } from 'react-router-dom'
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../firebase/firebase';
 
 function LoginPage() {
+  const navigate = useNavigate();
+  function loginShop ({email, password}) {
+    signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+    navigate('/')
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log('errorMessage ===', errorMessage);
+  });
+  }
   return (
-    <div>LoginPage</div>
+    <div>
+      <h3>Login Form</h3>
+      <LoginForm onLogin={loginShop} />
+    </div>
   )
 }
 
