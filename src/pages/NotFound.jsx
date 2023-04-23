e662ea0333157871efc27883bc95cf0f56226b5f
Armandas G <genysarmandas@gmail.com>
2023-04-23 17:48:33 +0300

    not found style added

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,17 +1,11 @@
-import React, { useEffect } from 'react'
-import { Link, useNavigate } from 'react-router-dom';
-import { useAuthCtx } from '../store/AuthProvider';
+import { Link } from 'react-router-dom';
+import './notFound.scss'
 
-function NotFound() {
-    const {isLoggedIn} = useAuthCtx()
-    const navigate = useNavigate()
-    
-    
-    
+function NotFound() {    
   return (
-    <div>
+    <div className='notFound'>
         <h1>Page not found 404</h1>
-        <Link to="/login">return</Link>
+        <Link className='return btn' to="/login">return</Link>
     </div>
   )
 }
